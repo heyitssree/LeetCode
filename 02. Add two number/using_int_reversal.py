ec48e34da676d7f005ebc119b7e3127547837d53
heyitssree <sreeamaravila@live.com>
2022-06-12 00:27:18 +0530

    Small fixes

--- 02. Add two number/using_int_reversal.py
+++ 02. Add two number/using_int_reversal.py
@@ -1,17 +1,21 @@
 def int_reverse(x: int) -> int:
+    """Methon to reverse the string."""
     reversed_int = 0
-    while(x!=0):
+    while((x != 0) or ((x % 10 == 0) and (x != 0))):
         digit = x % 10
-        reversed_int = reversed_int *10 + digit
+        reversed_int = reversed_int * 10 + digit
         x = x // 10
     return(reversed_int)
 
+
 class Solution:
+    """Checks if Number is Palindrome or not."""
     def isPalindrome(self, x: int) -> bool:
-        if(x<0):
+        """Checks if number is Palindrome."""
+        if(x < 0):
             return False
         else:
-            if(int_reverse(x)==x):
+            if(int_reverse(x) == x):
                 return True
             else:
                 return False
